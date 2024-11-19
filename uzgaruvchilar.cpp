49ef525011c2e38ca430687a8d9a538f9ba261df
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-19 11:20:20 +0500

    uzgaruvchilar(int and cin or serr)

--- uzgaruvchilar.cpp
+++ uzgaruvchilar.cpp
@@ -7,8 +7,8 @@ int main(void) {
 
   cerr << "Hello World" << endl; // cerr bu asosan malum funksiyda xato
                                  // chiqarish uchun ishlatiladi
-  int a = 12;
-  int b = 9;
+  int a = 12; // int 4 byt joy oladi
+  int b = 9; // int 4 byt joy oladi 
 
   cout << a + b << endl;
   cout << a - b << endl;
