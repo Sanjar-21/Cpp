f1934e4831f29f9546cc7618adc84c14d680a8f0
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-21 22:32:48 +0500

    floor, ceil, trunc, round

--- haqiqiy-sonlar.cpp
+++ haqiqiy-sonlar.cpp
@@ -9,5 +9,14 @@ int main() {
     cout << "floor(a) = " << floor(a) << endl; // 8
     cout << "floor(b) = " << floor(b) << endl; // 5
 
+    cout << "ceil(a) = " << ceil(a) << endl; // 9
+    cout << "ceil(b) = " << ceil(b) << endl; // 6
+
+    cout << "trunc(a) = " << trunc(a) << endl; // 8
+    cout << "trunc(b) = " << trunc(b) << endl; // 5
+
+    cout << "round(a) = " << round(a) << endl; // 8 yahlitlash
+    cout << "round(b) = " << round(b) << endl; // 6 yahlitlash
+
     return 0;
 }
