59968d11a6879833fbf483a8195ce1d19e039d6b
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-21 22:17:42 +0500

    haqiqiy sonlar

---
+++ haqiqiy-sonlar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    double a = 8.5;
+    float b = 5.5;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
