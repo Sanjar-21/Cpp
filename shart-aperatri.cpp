41039ed893997be235b713d10dd9e4e3142449c7
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-24 22:17:04 +0500

    if shart aperatiri

--- shart-aperatri.cpp
+++ shart-aperatri.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 int main(void) {
-    int n = 0;
-    cout << "n = ";
-    cin >> n;
+  int n = 0;
+  cout << "n = ";
+  cin >> n;
 
-    if(n > 0) {
-        cout << "kirtilgan son musbat son" << endl;
-    } else if(n == 0) {
-        cout << "n soni busbat ham manfiy ham emas \n";
-    } else {
-        cout << "kirtilgan son manfiy son" << endl;
-    }
+  if (n > 0) {
+    cout << "kirtilgan son musbat son" << endl;
+  } else if (n == 0) {
+    cout << "n soni busbat ham manfiy ham emas \n";
+  } else {
+    cout << "kirtilgan son manfiy son" << endl;
+  }
 
-    return (0);
+  return (0);
 }
