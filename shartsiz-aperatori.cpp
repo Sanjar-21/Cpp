6f2a282c99ee2daca2fe8b46513a98f40784c60f
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-25 08:53:02 +0500

    shartsiz aperotrlar goto

--- shartsiz-aperatori.cpp
+++ shartsiz-aperatori.cpp
@@ -3,14 +3,25 @@
 using namespace std;
 
 int main(void) {
-    int n = 0;
-nishon:
-    cout << "n = " ;
-    cin  >> n;
-    if(0 < n) {
-        cout << "n qabul qildi \n";
-    } else {
-        goto nishon;
+    // int n = 0;
+// nishon:
+    // cout << "n = " ;
+    // cin  >> n;
+    // if(0 < n) {
+    //     cout << "n qabul qildi \n";
+    // } else {
+    //     goto nishon;
+    // }
+
+    int n, m;
+qayta:
+    cout << "n = "; cin >> n;
+    cout << "m = "; cin >> m;
+
+    if(m != 0) {
+        cout << n / m << endl;
+    }else {
+        goto qayta;
     }
     return (0);
 }
