245a9712e2c8a872a3db24a25a34a57cd8792ad5
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-27 17:33:40 +0500

    masiv char cpp

---
+++ array-char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(void) {
+    char sss[] = "school-21";
+    for(int i = 0; i < strlen(sss); i++) {
+        cout << sss[i] << endl;
+    }
+    return (0);
+}
