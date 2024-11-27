2093acdce18863e12e098cac9623fc1ed7df2830
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-27 08:56:53 +0500

    do while cpp

---
+++ do-while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    do { // do wile birnchi oylandi va kiyin shartni tekshiradi     
+        cout << "N = ";
+        cin >> n;
+    } while(n >= 0);
+    cout << "hello cpp " << endl;
+
+    return 0;
+}
