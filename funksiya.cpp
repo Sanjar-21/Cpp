7f723296f80d2186ac1c9d338dca42e871f51ae3
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-27 09:07:24 +0500

    cpp da funksiya

--- funksiya.cpp
+++ funksiya.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
-int main(void) {
+int maximum () {
 
+    return 42;
+}
+
+string stringggg() {
+    return "4242424242424244242424 21 ";
+}
+
+int main(void) {
+    cout << maximum() << endl;
+    cout << stringggg() << endl;
     return  (0);
 }
