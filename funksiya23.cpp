193cb31675ae0a7c8c26215931e0ba0ffca4db0b
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-28 22:13:31 +0500

    funksiya void

--- funksiya23.cpp
+++ funksiya23.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 void printNumber(int n) { cout << n << endl; }
+void printFloat(float k) { cout << k << endl; }
 
 int main(void) {
   int k = 11;
+  float a = 2.1;
   printNumber(k);
+  printFloat(a);
   return 0;
 }
