b20568d8b8f28852566dc4d7b41390122417f534
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-30 22:37:42 +0500

    array cpp

--- array.cpp
+++ array.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
-int  main(void) {
-    int a[] = {1,2,4,5,6,7,78};
-    int size = sizeof(a) / sizeof(int);
+int main(void) {
+  int a[] = {1, 2, 4, 5, 6, 7, 78};
+  int size = sizeof(a) / sizeof(int); // bunda bu 7 ga teng
 
-    for(int i = 0; i < size; i++) {
-        cout << "a[" << i << "] = " << a[i] << endl;
-    }
+  for (int i = 0; i < size; i++) {
+    cout << "a[" << i << "] = " << a[i] << endl;
+  }
 
-    return (0);
+  return (0);
 }
