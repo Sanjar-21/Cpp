84a47dd322229f47eefe9d32f49cd629eccc6706
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-01 12:04:57 +0500

    class larni filar ga kuchirish

---
+++ files/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+int main(void) {
+    Main s;
+    s.salom();
+    return 0;
+}
---
+++ files/main.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using  namespace std;
+
+class Main {
+
+public:
+    void salom() {
+        cout << "jelladasdfa" << endl;
+    }
+};
