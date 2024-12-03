2fc984064537c1eb822832af38609f2a95c5888e
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-03 21:11:34 +0500

    array char

--- array-char.cpp
+++ array-char.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main(void) {
-    char sss[] = "school-21";
-    for(int i = 0; i < strlen(sss); i++) {
-        cout << sss[i] << endl;
-    }
-    return (0);
+  char sss[] = "school-21";
+  for (int i = 0; i < strlen(sss); i++) {
+    cout << sss[i] << endl;
+  }
+  return (0);
 }
