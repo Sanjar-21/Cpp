5a5625ccde662aa00878b6906e8054411a765837
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-04 22:47:44 +0500

    class filar bilan ishlash

---
+++ files/file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "file.h"
+
+using namespace std;
+
+filebilanishlash::filebilanishlash() {
+
+}
+
+filebilanishlash::~filebilanishlash() {
+    
+}
+
+int main(void) {
+
+    return 0;
+}
---
+++ files/file.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class filebilanishlash{
+
+private:
+    string name;
+
+public:
+    filebilanishlash();
+    ~filebilanishlash();
+    void setName(string str);
+    string getName();
+};
+
