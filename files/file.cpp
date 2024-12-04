3cbfd06173a03f45eecef0a9bbd526546288e55d
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-04 22:52:01 +0500

    calls file and cpp

--- files/file.cpp
+++ files/file.cpp
@@ -10,6 +10,14 @@ filebilanishlash::filebilanishlash() {
 filebilanishlash::~filebilanishlash() {
     
 }
+// classni filga chaqarish bu :: -> orqali ifodalandi?
+void filebilanishlash::setName(string abs) {
+    name = abs;
+}
+
+string filebilanishlash::getName() {
+    return name;
+}
 
 int main(void) {
 
