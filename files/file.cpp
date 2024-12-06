0a011ff8b9c46bfdfd9b8911ae5bbb5966f9b3db
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-06 22:03:54 +0500

    disctraktor class

--- files/file.cpp
+++ files/file.cpp
@@ -1,23 +1,15 @@
-#include <iostream>
 #include "file.h"
+#include <iostream>
 
 using namespace std;
 
-filebilanishlash::filebilanishlash() {
-    cout << "konsiktur" << endl;
-}
+filebilanishlash::filebilanishlash() { cout << "konsiktur" << endl; }
 
-filebilanishlash::~filebilanishlash() {
-    cout << "disktuktir" << endl;
-}
+filebilanishlash::~filebilanishlash() { cout << "disktuktir" << endl; }
 // classni filga chaqarish bu :: -> orqali ifodalandi?
-void filebilanishlash::setName(string abs) {
-    name = abs;
-}
+void filebilanishlash::setName(string abs) { name = abs; }
 
-string filebilanishlash::getName() {
-    return name;
-}
+string filebilanishlash::getName() { return name; }
 
 // int main(void) {
 //     filebilanishlash d;
