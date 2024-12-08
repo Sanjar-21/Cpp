4e087919410cd7d41cc62eaac54ac4f4770f81b5
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-08 22:09:14 +0500

    for loop in cpp

--- forloop.cpp
+++ forloop.cpp
@@ -2,20 +2,21 @@
 
 using namespace std;
 void forfor(int n) {
-    for(int i = 0; i <= n; i++) {
-        cout << i << endl;
-    }
+  for (int i = 0; i <= n; i++) {
+    cout << i << endl;
+  }
 }
 
 int main(void) {
-    // for(int i = 0; i <= 10; i++) {
-    //     cout << i << endl;
-    // }
-    int sum = 0;
-    for(int i = 0; i <= 10; i++) {
-        sum = sum + i ; cout << sum << endl;
-    }
+  // for(int i = 0; i <= 10; i++) {
+  //     cout << i << endl;
+  // }
+  int sum = 0;
+  for (int i = 0; i <= 10; i++) {
+    sum = sum + i;
+    cout << sum << endl;
+  }
 
-    forfor(8);
-    return 0;
+  forfor(8);
+  return 0;
 }
