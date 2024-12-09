80bb75e30817b75fc1968509b5e81b8cdc75dec5
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-09 17:04:47 +0500

    palindrom sonlar aniqlash cpp

--- palindrom-sonlar.cpp
+++ palindrom-sonlar.cpp
@@ -3,24 +3,24 @@
 using namespace std;
 
 int main(void) {
-    int n, number, d;
-    int reverse = 0;
+  int n, number, d;
+  int reverse = 0;
 
-    cout << "n : kritig: ";
-    cin >> n ;
-    number = n;
+  cout << "n : kritig: ";
+  cin >> n;
+  number = n;
 
-    do {
-        d = number % 10;
-        reverse = (reverse * 10) + d;
-        number = number / 10;
+  do {
+    d = number % 10;
+    reverse = (reverse * 10) + d;
+    number = number / 10;
 
-    } while(number != 0);
+  } while (number != 0);
 
-    if(n == reverse) {
-        cout << "bu palindrom" << endl;
-    } else {
-        cout << "bu shunchake raqam" << endl;
-    }
-    return 0;
+  if (n == reverse) {
+    cout << "bu palindrom" << endl;
+  } else {
+    cout << "bu shunchake raqam" << endl;
+  }
+  return 0;
 }
